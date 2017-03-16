577e78ebbad773688a61ef00ce306a50b8cb015e
Yusuke Kato <mikan1212.lion2a4p8c3@docomo.ne.jp>
2017-03-16 07:20:32 +0000

    HTML追加

---
+++ HTML_js/HTML003.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+    document.getElementById("text003").textContent ='\
+      ===========================\n\
+      script,cssで操作するために使う\n\
+      id="ID_NAME" id指定 何でもいける\n\
+      type="TYPE_NAME" type指定\n\
+      ===========================\n\
+      <!DOCTYPE HTML> ドキュメントのタイプ指定\n\
+      <html></html> HTML文書であると宣言\n\
+      <head></head> ヘッダ情報\n\
+      <meta></meta> メタ情報\n\
+      <title></title> ページのタイトル\n\
+      <body></body> 中身\n\
+      ===========================\n\
+      <h1></h1> 見出し 範囲は１から６,１が大きい\n\
+      <p></p> 段落（パラグラフ）\n\
+      <pre><pre> まとめて書ける\n\
+      <code></code> コード書けるらしい\n\
+      <font size=7></font> フォントのサイズを指定,数字が大きいほど大きい\n\
+      <script src="SCRIPT_FILE"></script> スクリプト指定\n\
+      <div></div> 区画\n\
+       ';
+})();
