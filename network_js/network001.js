eda459374906c6281090172ec9cc15b4c9438422
Yusuke Kato <mikan1212.lion2a4p8c3@docomo.ne.jp>
2017-04-07 04:56:54 +0000

    追加

---
+++ network_js/network001.js
@@ -0,0 +1,7 @@
+'use strict';
+document.getElementById('text001').textContent = "\
+ISO = 国際標準化機構 International Organization for Standardization （関係ない）\n\
+ISP = Internet Service Provider\n\
+LAN = Local Area Network\n\
+WAN = Wide Area Network\n\
+";
---
+++ secure_js/secure001.js
@@ -0,0 +1,6 @@
+'use strict';
+document.getElementById('text001').textContent = "\
+セキュリティ\n\
+secure\n\
+security\n\
+";
